d801b85c5f6f9af0f074053cda29f441696ff47b
terjanq <terjanq@gmail.com>
2016-09-10 18:53:29 +0200

    final

--- scripts/splix.js
+++ scripts/splix.js
@@ -1,43 +1,71 @@
 (function() {
 
-    window.addEventListener("load", function(){
-
-      ga('create', 'UA-78233995-2', 'auto', 'terjanq');
-      ga('terjanq.send', 'pageview', '/chrome');
-
-      setTimeout(function(){
-
-          MAX_ZOOM = 10000;
-          //BLOCKS_ON_SCREEN = 10000;
-          USERNAME_SIZE = 6;
-      }, 2000);
+      window.addEventListener("load", function(){
 
 
-      document.body.addEventListener("mousewheel", WheelHandler, true);
+        setTimeout(function(){
 
-     function WheelHandler(event) {
-         var zoom = BLOCKS_ON_SCREEN * Math.pow(0.93, event.wheelDelta / 120 || event.detail || 0);
-         if(zoom < 100) BLOCKS_ON_SCREEN = 100;
-         else if(zoom > 16000) BLOCKS_ON_SCREEN = 16000;
-         else BLOCKS_ON_SCREEN = zoom;
-      }
+            MAX_ZOOM = 10000;
+            //BLOCKS_ON_SCREEN = 10000;
+            USERNAME_SIZE = 6;
+        }, 2000);
 
 
+        document.body.addEventListener("mousewheel", WheelHandler, true);
 
-      var paused = false;
-      var loop = setInterval(function(){
-              if(paused) {
-                  sendDir((myPlayer.dir + 1)%4);
-              }
-          }, 100);
+       function WheelHandler(event) {
+           var zoom = BLOCKS_ON_SCREEN * Math.pow(0.93, event.wheelDelta / 120 || event.detail || 0);
+           if(zoom < 100) BLOCKS_ON_SCREEN = 100;
+           else if(zoom > 16000) BLOCKS_ON_SCREEN = 16000;
+           else BLOCKS_ON_SCREEN = zoom;
+        }
 
-      window.addEventListener("keyup", function(e){
-          if(e.keyCode == 80) paused ^= true;
-      });
+
+        var leaderboard = document.getElementById("leaderboard");
+        leaderboard.style.top = "48px";
+        var myCssText = "color:white; font-weight: 700; position:fixed; right:  13px; top: 8px; z-index:100; padding: 5px 10px; opacity: .7; transform-orign: right top;";
+
+        var myDiv = document.createElement("div");
+        myDiv.className  = "greenBox";
+        myDiv.style.cssText = myCssText;
+        myDiv.innerHTML = "terjanq.github.io/splix.io";
+
+
+        leaderboard.parentNode.insertBefore(myDiv, leaderboard.nextSibling);
 
 
 
+        var observerConfig = { attributes: true, attributeFilter: ['style'] };
 
 
-  });
-})();
+
+         var observer = new MutationObserver(function(mutations) {
+          mutations.forEach(function(mutation) {
+            if(mutation.type == "attributes" && mutation.attributeName == "style" && mutation.target === leaderboard) {
+              myDiv.style.cssText = leaderboard.style.cssText + ";" + myCssText;
+            }
+          });
+        });
+
+       observer.observe(leaderboard, observerConfig);
+
+
+        var paused = false;
+        var loop = setInterval(function(){
+                if(paused) {
+                    sendDir((myPlayer.dir + 1)%4);
+                }
+            }, 100);
+
+        window.addEventListener("keyup", function(e){
+            if(e.keyCode == 80) paused ^= true;
+        });
+
+        ga('create', 'UA-78233995-2', 'auto', 'ter');
+        ga('ter.send', 'pageview', '/chrome');
+
+
+
+
+    });
+  })();
